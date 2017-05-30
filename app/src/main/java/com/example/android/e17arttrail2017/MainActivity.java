05468d7f7a4683524186702551fa8eab2a4e1fd3
mbeevor <mbeevor@gmail.com>
2017-05-30 14:25:21 +0100

    Layout with navigation drawer

--- app/src/main/java/com/example/android/e17arttrail2017/MainActivity.java
+++ app/src/main/java/com/example/android/e17arttrail2017/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.android.e17arttrail2017;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] artActivities;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find resources for navigation drawer
+        artActivities = getResources().getStringArray(R.array.menu_list);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Create an adapter for the list view within navigation drawer
+        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, artActivities));
+
+//        // Set onClickListeners for each item in navigation drawer
+//        drawerList.setOnItemClickListener(new NavigationDrawer());
     }
 }
+
+
---
+++ app/src/main/java/com/example/android/e17arttrail2017/NavigationDrawer.java
@@ -0,0 +1,18 @@
+package com.example.android.e17arttrail2017;
+
+/**
+ * Class for selecting each item in the navigation drawer.
+ */
+
+//public class NavigationDrawer implements ListView.OnItemClickListener {
+//
+////    @Override
+////    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+////        selectItem(position);
+////    }
+////
+////    private void selectItem(int position) {
+////
+////    }
+//}
+
